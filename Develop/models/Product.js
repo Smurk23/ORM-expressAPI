6a44d84f70a0714b8f38104c745b3f05134257bf
Smurk23 <xingcantres@gmail.com>
2023-10-26 08:17:10 -0400

    product column is done

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -15,6 +15,11 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
  // define columns
   },
   {
